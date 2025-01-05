31366316bbd84e427b7ed1ed85afd99ece95b67e
ophiase <ophiase@outlook.com>
2025-01-05 02:59:44 +0100

    minor

--- download_kaggle.py
+++ download_kaggle.py
@@ -4,14 +4,13 @@ import os
 import numpy as np
 import shutil
 
-TARGET_FOLDER = os.path.join("data", "open_drug")
 
-def extract(target_folder: str = TARGET_FOLDER) -> str:
-    original_folder = kagglehub.dataset_download("mannbrinson/open-drug-knowledge-graph")
-    
+def extract(target_folder: str, kaggle_data: str) -> str:
+    original_folder = kagglehub.dataset_download(kaggle_data)
+
     if not os.path.exists(target_folder):
         os.makedirs(target_folder)
-    
+
     for item in os.listdir(original_folder):
         s = os.path.join(original_folder, item)
         d = os.path.join(target_folder, item)
@@ -19,11 +18,17 @@ def extract(target_folder: str = TARGET_FOLDER) -> str:
             shutil.copytree(s, d, dirs_exist_ok=True)
         else:
             shutil.copy2(s, d)
-    
+
     return target_folder
 
+
 def main() -> None:
-    extract()
+    for target_folder, kaggle_data in [
+        # ('pubmed', "krishnakumarkk/pubmed-knowledge-graph-dataset"), # too heavy
+        ("open_drug", "mannbrinson/open-drug-knowledge-graph")
+    ]:
+        extract(os.path.join("data", target_folder), kaggle_data)
+
 
 if __name__ == "__main__":
     main()
